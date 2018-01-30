d123b3e0a0d6b88f446a8fdac37f0323862f25b3
NextZeus <875697126@qq.com>
2018-01-30 23:15:15 +0800

    update

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+	private Vector3 startPos;
+	private Quaternion startRot;
+	// Use this for initialization
+	void Start () 
+	{
+		startPos = transform.position;
+		startRot = transform.rotation;
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.tag == "Death")
+		{
+			transform.position = startPos;
+			transform.rotation = startRot;
+			Debug.Log("Animator",GetComponent<Animator>());
+			GetComponent<Animator>().Play("LOSE00", 0, 0f);
+			GetComponent<Rigidbody>().velocity = new Vector3(0f,0f,0f);
+			GetComponent<Rigidbody>().angularVelocity = new Vector3(0f,0f,0f);
+		} else if (col.tag == "checkpoint")
+		{
+			startPos = col.transform.position;
+			startRot = col.transform.rotation;
+		}
+	}
+}
